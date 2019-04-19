8eb6954f80ee9621de1df38ce165d9a4b120a34a
Hiram Fleitas <hiramfleitas@hotmail.com>
2019-04-19 11:33:33 -0400

    Update Get-SQLUpdates.ps1

--- Get-SQLUpdates.ps1
+++ Get-SQLUpdates.ps1
@@ -1,32 +1,41 @@
-<#
-.SYNOPSIS
-Get-SQLPatches retrives list of SQL Patches (SP/CU/Hot-fixes) for a Computer.
-.DESCRIPTION
-Get-SQLPatches uses WMI to retrieve a list of the Win32_OperatingSystem BuildNumber, CSName.
-Get-SQLPatches uses Get-ChildItem to retrieve a list of DisplayName, DisplayVersion, InstallDate
-from path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall.
+function Get-SQLPatches {
+    <#
+        .SYNOPSIS
+            Retrives a historical list of all SQL Patches (CUs, Service Packs & Hot-fixes) installed on a Computer.
 
-Created By: Hiram Fleitas aka. DBA2.o @hiramfleitas
-Modified: 4/17/2019 05:09:35 PM  
-Version 1.0
+        .DESCRIPTION
+			Uses WMI to retrieve a list of the Win32_OperatingSystem BuildNumber, CSName.
+			Uses Get-ChildItem to retrieve a list of DisplayName, DisplayVersion, InstallDate from path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall.
 
-.PARAMETER ComputerName
-The Computer name to query. Default: Localhost.
-.EXAMPLE
-Get-SQLPatches -ComputerName HiramSQL1
-Gets the SQL Server patches from HiramSQL1
-.EXAMPLE
-Get-SQLPatches -ComputerName (Get-Content C:\Monitoring\Servers.txt)
-Gets the SQL Server patches from a list of computers in C:\Monitoring\Servers.txt.
-#>
-Function Get-SQLPatches {
+        .PARAMETER ComputerName
+            Allows you to specify a comma separated list of servers to query. Default: localhost.
+
+        .NOTES
+            Author: Hiram Fleitas, http://twitter.com/hiramfleitas, http://fleitasarts.com
+            Tags: Updates, Patches
+
+            Website: http://fleitasarts.com
+            Copyright: (c) 2019 by Hiram Fleitas, licensed under MIT
+            -           License: MIT https://opensource.org/licenses/MIT
+
+        .LINK
+            https://github.com/hfleitas/Monitoring/blob/master/Get-SQLUpdates.ps1
+
+        .EXAMPLE
+            PS C:\> Get-SQLPatches -ComputerName HiramSQL1, HiramSQL2
+
+            Gets a list of SQL Server patches installed on HiramSQL1 and HiramSQL2.
+
+        .EXAMPLE
+            PS C:\> Get-SQLPatches -ComputerName (Get-Content C:\Monitoring\Servers.txt)
+
+			Gets the SQL Server patches from a list of computers in C:\Monitoring\Servers.txt.
+			
+    #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$false,
-                        Position=0,
-                        ValueFromPipeline=$true,
-                        ValueFromPipelineByPropertyName=$true)]
-        [Alias("Name")]
+        [Parameter(Mandatory=$false, Position=0, ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [Alias("ServerInstance", "SqlServer", "SqlServers")]
         [string[]]$ComputerName=$env:COMPUTERNAME
         )
 
@@ -36,7 +45,24 @@ Function Get-SQLPatches {
         foreach ($Computer in $ComputerName) {
             try {
 			    $WMI_OS = Get-WmiObject -Class Win32_OperatingSystem -Property BuildNumber, CSName -ComputerName $Computer -ErrorAction Stop
-                
+				
+				$inst = Invoke-Command -computer $Computer {
+					(get-itemproperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances
+				}
+				
+				foreach ($i in $inst) {	
+				   $p = Invoke-Command -computer $Computer -ArgumentList $i {
+				   param($i)
+					   (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL').$i
+				    "{0}" -f $using:i
+				   }
+					$ver = Invoke-Command -computer $Computer -ArgumentList $p {
+					param($p)
+						(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$p\Setup").Version
+					"{0}" -f $using:p
+					}
+				}
+				
 				$Patches = Invoke-command -computer $Computer {
 					Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall `
 					| Get-ItemProperty `
@@ -49,32 +75,32 @@ Function Get-SQLPatches {
 				}
 				
 				foreach ($Patch in $Patches) {
-					## Creating Custom PSObject and Select-Object Splat
 					$List = @{
 						Property=(
 							'Computer',
 							'DisplayName',
+							'InstallDate',
 							'DisplayVersion',
-							'InstallDate'
-						)}
+							'InstanceVersion'
+						)
+					}
 					New-Object -TypeName PSObject -Property @{
-						Computer=$WMI_OS.CSName
-						DisplayName=$Patch.DisplayName
-						DisplayVersion=$Patch.DisplayVersion
-						InstallDate=$Patch.InstallDate
+						Computer		= $WMI_OS.CSName
+						DisplayName		= $Patch.DisplayName
+						InstallDate		= $Patch.InstallDate
+						DisplayVersion	= $Patch.DisplayVersion
+						InstanceVersion	= $ver
 					} | Sort-Object -Property @{Expression = "InstallDate"; Descending = $True} | Select-Object @List
-					} 
 				}
-				
-            catch [Exception] {
-                Write-Output "$computer $($_.Exception.Message)"
-                #return
-                }
+			} catch [Exception] {
+				Write-Output "$computer $($_.Exception.Message)"
+			}
         }
     }
     end{}
 }
 
+$local
 $Servers = Get-Content C:\Monitoring\Servers.txt
 $file = "C:\Monitoring\SQLPatches.txt"
 $Csv = "C:\Monitoring\SQLPatches.csv"
